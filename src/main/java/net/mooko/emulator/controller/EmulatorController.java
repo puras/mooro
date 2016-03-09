191eff134e3cef0c6c38736d45a2014ab276e4a2
puras <puras.he@gmail.com>
2016-03-09 16:25:30 +0800

    增加处理请求参数,将参数名与值合并进对应的JSON文件的文件名中; 增加请求内容与响应内容的日志打印（使用System.out）;

--- src/main/java/net/mooko/emulator/controller/EmulatorController.java
+++ src/main/java/net/mooko/emulator/controller/EmulatorController.java
@@ -9,9 +9,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.Map;
 
 /**
  * @author puras <he@puras.me>
@@ -40,6 +43,7 @@ public class EmulatorController {
     }
 
     private Object doRest(HttpServletRequest req) {
+        displayRequestInfo(req);
         String fileName = (getFileName(req));
         System.out.println("File Name is: " + fileName);
         InputStream is = this.getClass().getResourceAsStream("/data/" + fileName);
@@ -53,7 +57,10 @@ public class EmulatorController {
             StringWriter writer = new StringWriter();
             IOUtils.copy(is, writer, "UTF-8");
             json = writer.toString();
+            System.out.println("******************************");
+            System.out.println("Response Body: ");
             System.out.println(json);
+            System.out.println("******************************");
             Converter converter = new JacksonConverter(ObjectMapperHolder.getInstance().getMapper());
             Object obj = converter.convertToObject(json, Object.class);
             return obj;
@@ -80,6 +87,73 @@ public class EmulatorController {
         if (url.startsWith("_")) {
             url = url.substring(1);
         }
+
+        Map<String, String[]> params = req.getParameterMap();
+        String queryString = "";
+        for (Map.Entry<String, String[]> entry : params.entrySet()) {
+            String key = entry.getKey();
+            String value = "";
+            String[] values = entry.getValue();
+            if (null != values && values.length > 0) {
+                for (String val : values) {
+                    value += val;
+                }
+            }
+            queryString += key + "=" + value + "+";
+        }
+
+        if (queryString.endsWith("+")) {
+            queryString = queryString.substring(0, queryString.length() - 1);
+        }
+
+        if (null != queryString && queryString.length() > 0) {
+            url += "||" + queryString;
+        }
+
         return url.toLowerCase() + "-" + method.toLowerCase() + ".json";
+    }
+
+    private void displayRequestInfo(HttpServletRequest req) {
+        System.out.println("==============================");
+        System.out.println("Request URL:" + req.getRequestURI());
+        System.out.println("Request Method: " + req.getMethod());
+        System.out.println("------------------------------");
+        System.out.println("Request Header");
+        System.out.println("------------------------------");
+        Enumeration<String> enumeration = req.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String key = enumeration.nextElement();
+            System.out.println("Header[" + key + "]=" + req.getHeader(key));
+        }
+        System.out.println("------------------------------");
+        System.out.println("Request Query String");
+        System.out.println("------------------------------");
+        Map<String, String[]> params = req.getParameterMap();
+        for (Map.Entry<String, String[]> entry : params.entrySet()) {
+            String key = entry.getKey();
+            String value = "";
+            String[] values = entry.getValue();
+            if (null != values && values.length > 0) {
+                for (String val : values) {
+                    value += val;
+                }
+            }
+            System.out.println("QueryString[" + key + "]=" + value);
+        }
+        if (req.getMethod().toLowerCase().equals("post")) {
+            System.out.println("------------------------------");
+            System.out.println("Request Body");
+            System.out.println("------------------------------");
+            try {
+                BufferedReader reader = req.getReader();
+                String buffer = null;
+                while ((buffer = reader.readLine()) != null) {
+                    System.out.println(buffer);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("==============================");
     }
 }
